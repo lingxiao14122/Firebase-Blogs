c6feb4d48ffeff90cedf5ffbf1b0e65482e16103
ling <lingxiao3218@gmail.com>
2021-06-04 23:54:06 +0800

    Initial Commit

---
+++ src/store/index.js
@@ -0,0 +1,92 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sampleBlogCards: [
+      { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021", },
+      { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021", },
+      { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021", },
+      { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021", },
+    ],
+    // User
+    editPost: null,
+    user: null,
+
+    // Create Post State
+    blogHTML: "Write your blog title here ...",
+    blogTitle: "",
+    blogPhotoName: "",
+    blogPhotoFileUrl: null,
+    blogPhotoPreview: null,
+
+    // User Profile
+    profileAdmin: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileInitials: null,
+  },
+  mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    },
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+    setProfileAdmin(state, payload) {
+      state.profileAdmin = payload;
+      // console.log(state.profileAdmin);
+    },
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id;
+      state.profileEmail = doc.data().email;
+      state.profileFirstName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUserName = doc.data().username;
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFirstName.match(/(\b\S)?/g).join('') +
+        state.profileLastName.match(/(\b\S)?/g).join('');
+    },
+    changeFirstName(state, payload) {
+      state.profileFirstName = payload;
+    },
+    changeLastName(state, payload) {
+      state.profileLastName = payload;
+    },
+    changeUsername(state, payload) {
+      state.profileUsername = payload;
+    },
+  },
+  actions: {
+    async getCurrentUser({ commit }, user) {
+      const dataBase = await db.collection('users').doc(firebase.auth().currentUser.uid);
+      const dbResults = await dataBase.get();
+      commit("setProfileInfo", dbResults);
+      commit("setProfileInitials");
+
+      const token = await user.getIdTokenResult();
+      const admin = await token.claims.admin;
+      commit('setProfileAdmin', admin);
+    },
+    async updateUserSettings({commit, state}) {
+      const dataBase = await db.collection('users').doc(state.profileId);
+      await dataBase.update({
+        firstName: state.profileFirstName,
+        lastName: state.profileLastName,
+        username: state.profileUsername,
+      });
+      commit("setProfileInitials");
+    },
+  },
+  modules: {},
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="home">
+    <BlogPost v-if="!user" :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard
+            :post="post"
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-if="!user" class="updates">
+      <div class="container">
+        <h2>Never Miss a Post. Register a free account today!</h2>
+        <router-link class="router-button" to="#">
+          Register for Blogs <Arrow class="arrow arrow-light" />
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogPost from "../components/BlogPost";
+import BlogCard from "../components/BlogCard";
+import Arrow from "../assets/Icons/arrow-right-light.svg";
+export default {
+  name: "Home",
+  components: { BlogPost, BlogCard, Arrow },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo doloribus recusandae dolore quae maxime assumenda aliquam",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "title1 title1 ",
+          blogHTML: "TITLE",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "title2",
+          blogHTML: "TITLE",
+          blogCoverPhoto: "beautiful-stories",
+        },
+      ],
+    };
+  },
+  computed: {
+    sampleBlogCards() {
+      return this.$store.state.sampleBlogCards;
+    },
+    user() {
+      return this.$store.state.user;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+  }
+
+  .router-button {
+    display: flex;
+    font-size: 14;
+    text-decoration: none;
+    @media (min-width: 800px) {
+      margin-left: auto;
+    }
+  }
+
+  h2 {
+    font-weight: 300;
+    font-size: 32px;
+    max-width: 425px;
+    width: 100%;
+    text-align: center;
+    text-transform: uppercase;
+    @media (min-width: 800px) {
+      text-align: initial;
+      font-size: 40px;
+    }
+  }
+}
+</style>
